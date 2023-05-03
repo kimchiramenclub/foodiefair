025962d3936ebd84e2cf51dc2dd7e1e71d534911
seungyeon <devryuttung@gmail.com>
2023-05-03 21:25:54 +0900

    [Feat] 문의하기 모달창 추가 - userId 임시 전송

--- front-end/foodiefair-bootstrap/src/assets/js/ajax/registerCS.js
+++ front-end/foodiefair-bootstrap/src/assets/js/ajax/registerCS.js
@@ -1,13 +1,15 @@
-//문서 로딩 기다림
 $(document).ready(function () {
-    $("#register-cs").on("click", function (e) {
+    $("#csSubmit").on("click", function (e) {
         e.preventDefault();
 
-        const formData = {
-            inquiryType: $("input[name='radioRegister']:checked").attr("id"),
-            content: $(".register-content").val(),
-        };
+        let userId = 35;
 
+        const formData = {
+            userId: 35,
+            questionType: $("input[name='radioRegister']:checked").attr("id"),
+            questionDate: new Date(),
+            questionContent: $(".register-content").val(),
+        };
 
         $.ajax({
             url: "http://localhost:8081/api/cscenter/registercs",
@@ -15,11 +17,29 @@ $(document).ready(function () {
             contentType: "application/json",
             data: JSON.stringify(formData),
             success: function (response) {
-                console.log('Success:', response);
+                console.log("Success:", response);
+                if (response === "ok") {
+                    Swal.fire({
+                        icon: "success",
+                        html:
+                            "답변은 이메일로 전달해드립니다.</br>" +
+                            "처리까지 시간이 걸릴 수 있습니다. 감사합니다.",
+                        timer: 3000,
+                        confirmButtonColor: "#d63384",
+                    });
+                } else {
+                    Swal.fire({
+                        icon: "error",
+                        html:
+                            "불편을 드려 죄송합니다.</br>" +
+                            "잠시 후에 다시 시도해주세요.",
+                        timer: 3000,
+                        confirmButtonColor: "#d63384",
+                    });
+                }
             },
             error: function (xhr, status, error) {
-                console.log("Error submitting form");
-                console.error('Error:', error);
+                console.error("Error:", error);
             },
         });
     });
