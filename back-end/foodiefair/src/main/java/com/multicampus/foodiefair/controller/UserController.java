7c5cf1e9ad7aafd5c0ac77814cebed3a67de73bd
joon <june.w.lim@gmail.com>
2023-05-11 16:59:26 +0900

    [Feat] 마이페이지 기능들 Test

--- back-end/foodiefair/src/main/java/com/multicampus/foodiefair/controller/UserController.java
+++ back-end/foodiefair/src/main/java/com/multicampus/foodiefair/controller/UserController.java
@@ -259,7 +259,7 @@ public class UserController {
         }
     }
 
-    @GetMapping("/user-read/{userId}")
+        @GetMapping("/user-read/{userId}")
     public ResponseEntity<Map<String, Object>> userRead(
             @PathVariable("userId") int userId) {
 
---
+++ front-end/foodiefair-bootstrap/src/assets/js/ajax/sendUserIds.js
@@ -0,0 +1,29 @@
+let userId;
+let loginUserId;
+
+function getUserIdFromUrl() {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get("userId");
+}
+userId = getUserIdFromUrl();
+getUserInfo().then(data => {
+    loginUserId = data.userId;
+});
+console.log(loginUserId);
+
+fetch(`http://localhost:8081/user-read/${userId}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('userImg').src = data.userRead.userImg;
+        document.getElementById('userName').innerHTML = data.userRead.userName;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+const mypageSaved = document.getElementById('mypage-saved');
+mypageSaved.href = `${mypageSaved.href}?userId=${userId}`;
+const mypageReviews = document.getElementById('mypage-reviews');
+mypageReviews.href = `${mypageReviews.href}?userId=${userId}`;
+
+
