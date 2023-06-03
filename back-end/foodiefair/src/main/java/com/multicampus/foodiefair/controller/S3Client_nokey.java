a8ab79b19da152d2ce2c8d3a21e0bd805d59d1cd
joon <june.w.lim@gmail.com>
2023-06-04 02:22:52 +0900

    [Chore] key 있는 버전 gitignore 등록, nokey 버전 생성

---
+++ back-end/foodiefair/src/main/java/com/multicampus/foodiefair/controller/S3Client_nokey.java
@@ -0,0 +1,92 @@
+package com.multicampus.foodiefair.controller;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Date;
+
+public class S3Client_nokey {
+
+    private static final String ACCESS_KEY = "";     //네이버 클라우드 자격 증명
+    private static final String SECRET_KEY = "";
+    private static final String ENDPOINT = "";
+    private static final String REGION = "";
+    private static final String BUCKET_NAME = "";   //네이버 버킷 이름
+
+    private AmazonS3 s3Client;
+
+    public S3Client_nokey() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(ENDPOINT, REGION);
+
+        this.s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withEndpointConfiguration(endpointConfiguration)
+                .build();
+    }
+
+    //NPC Object Storage에 파일을 업로드하고 URL을 반환하는 메서드
+    public String uploadFile(File file, String objectKey) {
+        String folderPath = "products/";
+        PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, folderPath + objectKey, file);
+        s3Client.putObject(putObjectRequest);
+
+        URL url = s3Client.getUrl(BUCKET_NAME, folderPath + objectKey);
+        return (url != null) ? url.toString() : null;
+    }
+
+    public String uploadUserFile(File file, String objectKey) {
+        String folderPath = "users/";
+        PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, folderPath + objectKey, file);
+        s3Client.putObject(putObjectRequest);
+
+        URL url = s3Client.getUrl(BUCKET_NAME, folderPath + objectKey);
+        return (url != null) ? url.toString() : null;
+    }
+
+    //NPC Object Storage에 파일을 업로드하고 URL을 반환하는 메서드
+    public String uploadReviewFile(File file, String objectKey) {
+        String folderPath = "reviews/";
+        PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, folderPath + objectKey, file);
+        s3Client.putObject(putObjectRequest);
+
+        URL url = s3Client.getUrl(BUCKET_NAME, folderPath + objectKey);
+        return (url != null) ? url.toString() : null;
+    }
+
+    //이미 업로드된 파일에 대한 SignedUrl을 생성하는 메서드
+    public String getProductUrl(String objectKey, int expirationTimeInSeconds) {
+        String folderPath = "products/";
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(BUCKET_NAME, folderPath + objectKey)
+                .withMethod(HttpMethod.GET)
+                .withExpiration(new Date(System.currentTimeMillis() + expirationTimeInSeconds * 1000));
+        URL signedUrl = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return signedUrl.toString();
+    }
+
+    public String getUserUrl(String objectKey, int expirationTimeInSeconds) {
+        String folderPath = "users/";
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(BUCKET_NAME, folderPath + objectKey)
+                .withMethod(HttpMethod.GET)
+                .withExpiration(new Date(System.currentTimeMillis() + expirationTimeInSeconds * 1000));
+        URL signedUrl = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return signedUrl.toString();
+    }
+
+    public String getReviewUrl(String objectKey, int expirationTimeInSeconds) {
+        String folderPath = "reviews/";
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(BUCKET_NAME, folderPath + objectKey)
+                .withMethod(HttpMethod.GET)
+                .withExpiration(new Date(System.currentTimeMillis() + expirationTimeInSeconds * 1000));
+        URL signedUrl = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return signedUrl.toString();
+    }
+}
---
+++ back-end/foodiefair/src/main/java/com/multicampus/foodiefair/service/OCRServiceImpl_nokey.java
@@ -0,0 +1,119 @@
+package com.multicampus.foodiefair.service;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Service
+public class OCRServiceImpl_nokey implements OCRService {
+
+    private static final String API_URL = "";
+    private static final String SECRET_KEY = "";
+
+    public boolean sendOcrRequest(String base64Data, String productName) {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            con.setRequestProperty("X-OCR-SECRET", SECRET_KEY);
+
+            JSONObject json = new JSONObject();
+            json.put("version", "V2");
+            json.put("requestId", UUID.randomUUID().toString());
+            json.put("timestamp", System.currentTimeMillis());
+            JSONObject image = new JSONObject();
+            image.put("format", "png");
+            image.put("data", base64Data);
+            image.put("name", "testImg01");
+            JSONArray images = new JSONArray();
+            images.put(image);
+            json.put("images", images);
+            String postParams = json.toString();
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            JSONObject jsonResponse = new JSONObject(response.toString())
+                    .getJSONArray("images").getJSONObject(0)
+                    .getJSONObject("receipt")
+                    .getJSONObject("result")
+                    ;
+
+            return checkOcrResponse(jsonResponse, productName);
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean checkOcrResponse(JSONObject jsonResponse, String productName) {
+        // 1. "confirmNum" 이 존재하고, "confirmNum"의 "text" 값이 8자리
+        if (!jsonResponse.getJSONObject("paymentInfo").has("confirmNum")
+                || jsonResponse.getJSONObject("paymentInfo").getJSONObject("confirmNum").getString("text").length() != 8) {
+            return false;
+        }
+
+        // 2. productName 문자열이 "subResults"의 "items"의 "name"의 "text" 값을 포함하고 있는 지 (띄어쓰기는 무시하고)
+        JSONArray items = jsonResponse.getJSONArray("subResults").getJSONObject(0).getJSONArray("items");
+        boolean productFound = false;
+        String cleanedProductName = productName.replaceAll("\\s+", "");
+
+        for (int i = 0; i < items.length(); i++) {
+            JSONObject item = items.getJSONObject(i);
+            String nameText = item.getJSONObject("name").getString("text").replaceAll("\\s+", "");
+
+            if (cleanedProductName.contains(nameText)) {
+                productFound = true;
+                break;
+            }
+        }
+
+        if (!productFound) {
+            return false;
+        }
+
+        // 3. "paymentInfo"의 "date" "formatted"의 "month" 값과 "day" 값이 현재 날짜와 비교해서 30일 이내인지
+        JSONObject date = jsonResponse.getJSONObject("paymentInfo").getJSONObject("date").getJSONObject("formatted");
+        String month = date.getString("month");
+        String day = date.getString("day");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate parsedDate = LocalDate.parse("2023-" + month + "-" + day, formatter);
+        LocalDate currentDate = LocalDate.parse("2023-05-03", formatter);
+
+        long daysBetween = ChronoUnit.DAYS.between(parsedDate, currentDate);
+        return daysBetween <= 30;
+    }
+
+}
